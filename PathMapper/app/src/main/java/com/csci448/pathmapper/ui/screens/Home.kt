13e531cbc4f9065de2d7b0d59ddadaa70e663643
Tonythepigeon <tonythepigeonv.3@gmail.com>
2022-03-08 15:10:35 -0700

    Project created using MVVM concepts

---
+++ PathMapper/app/src/main/java/com/csci448/pathmapper/ui/screens/Home.kt
@@ -0,0 +1,14 @@
+package com.csci448.pathmapper.ui.screens
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Preview(showBackground = true)
+@Composable
+private fun PreviewCharacterDetailScreen(){
+    Column(){
+        Text("Home Screen") //....
+    }
+}
